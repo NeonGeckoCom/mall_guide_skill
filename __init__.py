5ce5676d8851a9c0515f70cf6d70ffd19fe44ee4
mariiarazno <mariia@neon.ai>
2022-09-18 14:43:17 +0200

    ask user for sort celection, time selection functions, better user request and shop names match, docstrings

--- __init__.py
+++ __init__.py
@@ -40,6 +40,7 @@ class DirectorySkill(NeonSkill):
         super(DirectorySkill, self).__init__(name="DirectorySkill")
         self.request_handler = RequestHandler()
         self.cache = dict()
+        self.url = "https://www.alamoanacenter.com/en/directory/"
 
 
     def initialize(self):
@@ -50,7 +51,6 @@ class DirectorySkill(NeonSkill):
     @intent_file_handler("run_mall_parser.intent")
     def start_mall_parser_intent(self, message):
         LOG.info(message.data)
-
         self._start_mall_parser_prompt(message)
         return
 
@@ -92,51 +92,186 @@ class DirectorySkill(NeonSkill):
             return None
 
     def speak_shops(self, shop_info):
+        """
+        Speaks shop info that was found.
+        Substitutes time format for better pronunciation.
+        speak_dialog('found_shop', {"name": shop['name'], "hours": hours, "location": location})
+        Shows shop label image.
+        Args:
+            shop_info (list): found shops on user's
+                                request
+        """
         for shop in shop_info:
             LOG.info(shop)
             location = self.request_handler.location_format(shop['location'])
-            hours = re.sub('(\d+)am(.+\d)pm', r'\1 A M\2 P M', shop['hours'])
+            hours = re.sub('(\d+)am(.+\d)pm', r'\1 A M \2 P M', shop['hours'])
             self.speak_dialog('found_shop', {"name": shop['name'], "hours": hours, "location": location})
-            print({"name": shop['name'], "hours": hours, "location": location})
+            LOG.info({"name": shop['name'], "hours": hours, "location": location})
             #self.gui.show_image(shop['logo'])
+
+    def location_selection(self, shop_info):
+        """
+        If there are several shops in found shops list
+        and user wants to get shop info on the certain
+        floor. If shop on that floor exists speaks
+        this shop info. Else speaks all shops info.
+        Args:
+            shop_info (list): found shops on user's
+                                request
+        Returns:
+            3, None (to ask for another shop info)
+        """
+        LOG.info(f"Shop by location selection {shop_info}")
+        floor = self.get_response('which_floor')
+        shops = self.request_handler.shop_selection_by_floors(floor, shop_info)
+        if shops:
+            self.speak_shops(shops)
+            return 3, None
+        else:
+            self.speak_dialog('no_shop_on_level')
+            self.speak_shops(shop_info)
+            return 3, None
+
+    def open_shops_search(self, shop_info):
+        """
+       Selects open shops. Collects the list of
+       open shops else return empty list.
+       Args:
+           shop_info (list): found shops on user's
+                               request
+       Returns:
+           shop_info (list): open shops
+       """
+        open_shops = []
+        day_time, hour, min = self.request_handler.curent_time_extraction()
+        for shop in shop_info:
+            parse_time = shop['time'].split('-')
+            open_time = int(re.sub('[^\d+]', '', parse_time[0]))
+            close_time = int(re.sub('[^\d+]', '', parse_time[1]))
+            if open_time <= hour < close_time:
+                open_shops.append(shop)
+        return open_shops
+
+
+    def time_calculation(self, shop_info, open):
+        # add logic if shop opens and closes not at am-pm time period
+        """
+        Calculates time difference between user's current time
+        and shop working hours.
+        If shop is closed and user is before opening speaks how
+        much time is left for waiting. If user's time is evening
+        speaks when the shop opens in the morning. In other cases
+        just speaks shops info.
+        If user in open hours, speak the shop info. If shop open
+        and closes in one hour speaks how many minutes left.
+        Args:
+            shop_info (list): found shops on user's request
+            open (boolean): True - if shop is open
+        Returns:
+         3, None (to ask for another shop info)
+        Examples:
+            work time 9am-10pm
+            user's time 8am
+            Prompt: 'Shop is closed now. Opens in 1 hour'
+        """
+        for shop in shop_info:
+            work_time = shop['time']
+            shop_name = shop['name']
+            day_time, hour, min = self.request_handler.curent_time_extraction()
+            parse_time = work_time.split('-')
+            open_time = int(re.sub('[^\d+]', '', parse_time[0]))
+            close_time = int(re.sub('[^\d+]', '', parse_time[1]))
+            # time left
+            wait_h = open_time - hour - 1
+            wait_min = 60 - min
+            if open:
+                if day_time[1] == 'pm' and 0 >= (close_time - hour) <= 1:
+                    self.speak(f'{shop_name} closes in {wait_min} minutes')
+                self.speak_shops([shop])
+            else:
+                if day_time[1] == 'am' and hour < open_time:
+                    self.speak(f'{shop_name} is closed now. Opens in {wait_h} hour and {wait_min} minutes')
+                elif hour >= close_time:
+                    self.speak(f'{shop_name} is closed now. Shop opens at {open_time}')
+                self.speak_shops([shop])
         return 3, None
 
-    def more_than_one(self, shop_info):
-        self.speak_dialog('more_than_one')
-        speak_all_shops = self.ask_yesno('speak_all_shops')
-        if speak_all_shops == 'yes':
-            self.speak_dialog('all_locations')
-            return self.speak_shops(shop_info)
+    def shops_by_time_selection(self, shop_info):
+        """
+        If user chose to select shops by time or
+        use like default selection. Selects open
+        shops.
+        Args:
+           shop_info (list): found shops on user's
+                               request
+        Returns:
+           shop_info (list): open shops
+        """
+        LOG.info(f"Shop by time selection {shop_info}")
+        open_shops = self.open_shops_search(shop_info)
+        if len(open_shops) >= 1:
+            return self.time_calculation(open_shops, True)
         else:
-            shop_by_floor = self.ask_yesno('shop_by_floor')
-            if shop_by_floor == 'yes':
-                floor = self.get_response('which_floor')
-                shop = self.request_handler.shop_selection_by_floors(floor, shop_info)
-                if shop is not None:
-                    return self.speak_shops([shop])
-                else:
-                    self.speak_dialog('no_shop_on_level')
-                    return self.speak_shops(shop_info)
-            else:
-                self.speak_dialog('all_locations')
-                return self.speak_shops(shop_info)
+            return self.time_calculation(shop_info, False)
 
     def find_shop(self, user_request, mall_link):
+        """
+        When the intent is matched, user_request
+        variable contains the name of the shop.
+        The matching function get_shop_data() is
+        used to find the shop name in cache or
+        on the mall page.
+        If user's request is not None this function
+        can return several shops, one shop or empty
+        list.
+        If no shop was found asks user to repeat.
+        returns 1, user_request to continue the
+        execution loop in self.execute().
+        If there are several shops asks user what way
+        of sorting to choose: time, level, nothing.
+            If 'time' - finds open shops. If open shops
+            list is not empty speaks open shops, else
+            tells time difference between user and shops'
+            work hours.
+            If 'location' - asks what level user is interested
+            in. If shops were found speaks shops' info,
+            else tells that there is no shop on that level
+            and speaks all found shops.
+            If  'no' - sorts by time.
+            If nothing matched in the answer - sorts by time.
+        If there was one shop found speaks this
+        shop info. Returns 3, None to stop current
+        shop search.
+        Location and time sorting functions return
+        3, None to stop current shop search.
+        """
         LOG.info(str(user_request))
         LOG.info(str(mall_link))
         if user_request is not None:
             self.speak_dialog(f"I am parsing shops and malls for your request")
             LOG.info(f"I am parsing shops and malls for your request")
-            shop_info = self.request_handler.get_shop_data(mall_link, user_request, self.cache)
+            shop_info = self.request_handler.get_shop_data(mall_link, user_request)
             if len(shop_info) == 0:
                 self.speak_dialog("shop_not_found")
                 user_request = self.get_response('repeat')
                 return 1, user_request
             elif len(shop_info) > 1:
-                return self.more_than_one(shop_info)
+                self.speak_dialog('more_than_one')
+                # ask for the way of selection: time, location, nothing
+                sorting_selection = self.get_response('Do you want to select'
+                                                      'store by time or location?')
+                if sorting_selection.contains('time'):
+                    return self.shops_by_time_selection(shop_info)
+                elif sorting_selection.contains('location'):
+                    return self.location_selection(shop_info)
+                elif sorting_selection.contains('no'):
+                    return self.shop_by_time_selection(shop_info)
+                else:
+                    return self.shop_by_time_selection(shop_info)
             else:
                 LOG.info(f"found shop {shop_info}")
-                return self.speak_shops(shop_info)
+                self.speak_shops(shop_info)
+                return 3, None
         else:
             LOG.info(str(None))
             return 3, None
--- request_handling.py
+++ request_handling.py
@@ -5,19 +5,90 @@ from neon_utils.skills.neon_skill import LOG
 import urllib.request
 
 import lingua_franca
+from lingua_franca import parse
 from lingua_franca.format import pronounce_number
 lingua_franca.load_language('en')
 
 import re
+from os import path
+import json
+
+from datetime import datetime
+import pytz
 
 
 class RequestHandler():
 
     def __init__(self) -> None:
-        pass
-        
+        self.caching_file = path.join(path.abspath(path.dirname(__file__)),
+                                  'cached_stores.json')
 
-    def existing_lang_check(user_lang, url):
+    def find_cached_stores(self, user_request: str):
+        """
+        Check shop name existence in cache keys
+        Args:
+            user_request (str): shop from user's message
+        Returns:
+            if file is empty -> None, {}
+            if shop wasn't found -> None, read data
+            if shop found ->  store_info (list), read data
+        Examples:
+            [
+            {"name": "ABS stores", "time": "8am-10pm", "location": "1 level"},
+            {"name": "ABS stores", "time": "8am-10pm", "location": "2 level"}
+            ]
+        """
+        with open(self.caching_file, 'r') as readfile:
+            file_length = os.stat(self.caching_file).st_size
+            if file_length == 0:
+                LOG.info('No shop in cache')
+                return None, {}
+            else:
+                data = json.load(readfile)
+                found_key = [key for key in data.keys() if key in user_request or user_request in key]
+                if user_request is []:
+                    LOG.info("Shop doesn't exist in cache")
+                    return None, data
+                else:
+                    LOG.info('Shop exists')
+                    return data[found_key], data
+
+    def caching_stores(self, data, store_info: list):
+        """
+        Saves dictionary to JSON file
+        key - shop name, value: list with shops info
+        if file is empty -> creates dictionary
+        if file contains info -> updates data dictionary
+        with the new shop info
+        Args:
+            data (dict): existing shops info
+            store_info (list): scraped store info
+        Returns:
+            store_info (list): scraped store info
+        Examples:
+            {"ABS stores": [
+            {"name": "ABS stores", "time": "8am-10pm", "location": "1 level"},
+            {"name": "ABS stores", "time": "8am-10pm", "location": "2 level"}
+            ]}
+        """
+        if data == {}:
+            data = {store_info['name']: store_info}
+        else:
+            new_store = {store_info['name']: store_info}
+            data.update(new_store)
+        with open(self.caching_file, 'w') as outfile:
+            json.dump(data, outfile)
+        return store_info
+
+    def existing_lang_check(user_lang: str, url):
+        """
+        Check existence of user's language
+        on the mall web-page
+        Args:
+            user_lang (str): user's lang in ISO 639-1
+        Returns:
+            bool: True if lang exists
+        """
         link = url+user_lang+'/directory/'
         response = requests.get(link)
         if response.status_code == 200:
@@ -27,8 +98,77 @@ class RequestHandler():
             LOG.info('This language is not supported')
             return False, link
 
+    def curent_time_extraction(self):
+        """
+        Defines current time in utc timezone
+        Format: hour:minutes part of day (1:23 pm)
+
+        Returns:
+            day_time (list): contains splited time
+                                numerals and part of the day
+                                day_time -> ['07:19', 'am']
+            hour (int): current hour
+            min (int): current minute
+        """
+        now = datetime.utcnow().replace(tzinfo=pytz.utc).strftime("%H:%M %p")
+        day_time = now.lower().split(' ')
+        exact_time = day_time[0].split(':')
+        hour, min = int(exact_time[0]), int(exact_time[1])
+        return day_time, hour, min
+
+    def location_format(self, location):
+        """
+        Finds all digits in store's location and
+        formats them to numeral words.
+        Args:
+            location (str): location info
+            from shops info
+        Returns:
+            if digits were found:
+                pronounced (str): utterance with
+                pronounced digits
+            else:
+                location (str): not changed utterance
+        Examples:
+            'level 1' -> 'level one'
+        """
+        floor = re.findall(r'\d+', location)
+        if len(floor) > 0:
+            floor = floor[0]
+            num = pronounce_number(int(floor), ordinals=False)
+            pronounced = re.sub(r'\d+', num, location)
+            return pronounced
+        else:
+            return location
+
+    def shop_selection_by_floors(self, user_request, found_shops):
+        """
+        If there are several shops in found shops list
+        and user agrees to select shop by floor.
+        Finds all digits in store's location and
+        formats them to ordinal and cardinal numerals.
+        Matches formated numerals with user's request.
+        If shop was found appends it to the new found
+        list.
+        Args:
+            user_request (str): floor from user
+            found_shops (list): found shops on user's
+            request
+        Returns:
+            shops_by_floor (list): shops that was found by floor
+        """
+        shops_by_floor = []
+        for shop in found_shops:
+            numbers = re.findall(r'\d+', shop['location'])
+            if len(numbers) > 0:
+                numbers = numbers[0]
+                num = pronounce_number(int(numbers), ordinals=False)
+                num_ordinal = pronounce_number(int(numbers), ordinals=True)
+                if num in user_request or num_ordinal in user_request:
+                    shops_by_floor.append(shop)
+        return shops_by_floor
+
     def parse(self, url):
-        url = "https://www.alamoanacenter.com/en/directory/"
         headers = {
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
             }
@@ -41,41 +181,47 @@ class RequestHandler():
         except HTTPError:
             LOG.info("Failed url parsing")
 
-    def location_format(self, location):
-        floor = re.findall(r'\d+', location)
-        if len(floor) > 0:
-            floor = floor[0]
-            num = pronounce_number(int(floor), ordinals=False)
-            pronounced = re.sub(r'\d+', num, location)
-            return pronounced
+
+    def get_shop_data(self, url, user_request):
+        """
+        Check existence of user's request store in cache
+        if shop was found returns list with shop info,
+        else does parsing of mall's web-page.
+        Matches the name of existing stores with user's
+        request. If store was found, returns list with
+        stores' info and does caching, else returns empty
+        list.
+        on the mall web-page
+        Args:
+            url (str): mall link from hardcoded in init.py
+            user_request (str): utterance from stt parsing
+        Returns:
+            : found_shops (list): found shops' info
+        """
+        # search for store existence in cache
+        found_shops, data = self.find_cached_stores(user_request)
+        if found_shops is not None:
+            return found_shops
         else:
-            location
-
-
-    def get_shop_data(self, url, user_request, cache):
-        found_shops = []
-        soup = self.parse(url)
-        for shop in soup.find_all(attrs={"class": "directory-tenant-card"}):
-            logo = shop.find_next("img").get('src')
-            info = shop.find_next(attrs={"class": "tenant-info-container"})
-            name = info.find_next(attrs={"class": "tenant-info-row"}).text.strip().strip('\n')
-            if name.lower() in user_request.lower() or user_request.lower() in name.lower():
-                hours = info.find_next(attrs={"class": "tenant-hours-container"}).text.strip('\n')
-                location = info.find_next(attrs={"tenant-location-container"}).text.strip('\n')
-                shop_data = {'name': name, 'hours': hours, 'location': location, 'logo': logo}
-                found_shops.append(shop_data)
-        return found_shops
-
-
-    def shop_selection_by_floors(self, user_request, found_shops):
-        for shop in found_shops:
-            numbers = re.findall(r'\d+', shop['location'])
-            if len(numbers) > 0:
-                numbers = numbers[0]
-                num = pronounce_number(int(numbers), ordinals=False)
-                num_ordinal = pronounce_number(int(numbers), ordinals=True)
-                if num in user_request or num_ordinal in user_request:
-                    return shop
+            # parsing mall web-page
+            found_shops = []
+            soup = self.parse(url)
+            # loop through store names
+            for shop in soup.find_all(attrs={"class": "directory-tenant-card"}):
+                logo = shop.find_next("img").get('src')
+                info = shop.find_next(attrs={"class": "tenant-info-container"})
+                name = info.find_next(attrs={"class": "tenant-info-row"}).text.strip().strip('\n')
+                # matching store names with user's request
+                if name.lower() in user_request.lower() or user_request.lower() in name.lower():
+                    hours = info.find_next(attrs={"class": "tenant-hours-container"}).text.strip('\n')
+                    location = info.find_next(attrs={"tenant-location-container"}).text.strip('\n')
+                    shop_data = {'name': name, 'hours': hours, 'location': location, 'logo': logo}
+                    found_shops.append(shop_data)
+            if found_shops:
+                # caching if shop was found
+                self.caching_stores(data, found_shops)
+                return found_shops
             else:
-                None
+                # return empty list
+                return found_shops
 
